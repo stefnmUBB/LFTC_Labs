b29925fbb48682da8034304c252da664821d4677
stefnmUBB <liviu.neacsu@stud.ubbcluj.ro>
2024-01-14 12:26:19 +0200

    reorganized repo

--- Tema6/mlpcomp.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-main.exe test\main.asm <examples\%1.mlpc || goto :END
-pushd test
-call compile.bat 
-popd
-
-test\main.exe
-
-:END
--- Tema6/test/compile.bat
+++
@@ -1,7 +0,0 @@
-nasm.exe -fobj main.asm -l main.lst -I.\ || goto :END
-
-ALINK.EXE -oPE -subsys console -entry start main.obj || goto :END
-
-:END
-
-echo Done.
